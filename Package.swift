12eccf4ff5588e93d42206b0b52aeb87c9a11010
Andrey Torlopov <torlopov.mail@ya.ru>
2024-10-04 12:24:52 +0300

    .

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "PackageDemo2",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "PackageDemo2",
+            targets: ["PackageDemo2"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "PackageDemo2"),
+        .testTarget(
+            name: "PackageDemo2Tests",
+            dependencies: ["PackageDemo2"]),
+    ]
+)
